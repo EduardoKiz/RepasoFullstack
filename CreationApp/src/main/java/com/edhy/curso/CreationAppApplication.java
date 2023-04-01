7fab8a6328e1fb7ae58d4db83642e3e8377949c3
EduardoKiz <98929183+EduardoKiz@users.noreply.github.com>
2023-04-01 01:02:16 -0600

    add verison 0 without change,
    add verison 0 without change,

---
+++ CreationApp/src/main/java/com/edhy/curso/CreationAppApplication.java
@@ -0,0 +1,13 @@
+package com.edhy.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreationAppApplication.class, args);
+	}
+
+}
---
+++ CreationApp/src/test/java/com/edhy/curso/CreationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edhy.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
